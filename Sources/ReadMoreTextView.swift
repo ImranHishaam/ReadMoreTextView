e9a725180c89531c1528e1a3aaa293ff899a71a5
imran.hishaam <imran.hishaam@s60.com.au>
2018-04-11 12:11:46 +0530

    string extension removed

--- Sources/ReadMoreTextView.swift
+++ Sources/ReadMoreTextView.swift
@@ -47,8 +47,8 @@ public class ReadMoreTextView: UITextView {
         isEditable = false
         
         let attributedDefaultReadMoreText = NSAttributedString(string: defaultReadMoreText, attributes: [
-            NSAttributedStringKey(rawValue: kCTForegroundColorAttributeName as String as String): UIColor.lightGray,
-            NSAttributedStringKey(rawValue: kCTFontAttributeName as String as String): font ?? UIFont.systemFont(ofSize: 14)
+            kCTForegroundColorAttributeName as String: UIColor.lightGray,
+            kCTFontAttributeName as String: font ?? UIFont.systemFont(ofSize: 14)
             ])
         attributedReadMoreText.append(attributedDefaultReadMoreText)
         self.attributedReadMoreText = attributedReadMoreText
@@ -237,8 +237,8 @@ public class ReadMoreTextView: UITextView {
     
     private func attributedStringWithDefaultAttributes(from text: String) -> NSAttributedString {
         return NSAttributedString(string: text, attributes: [
-            NSAttributedStringKey(rawValue: kCTFontAttributeName as! String): font ?? UIFont.systemFont(ofSize: 14),
-            NSAttributedStringKey(rawValue: kCTForegroundColorAttributeName as! String): textColor ?? UIColor.black
+            kCTFontAttributeName as String: font ?? UIFont.systemFont(ofSize: 14),
+            kCTForegroundColorAttributeName as! String: textColor ?? UIColor.black
             ])
     }
     
@@ -363,12 +363,5 @@ public class ReadMoreTextView: UITextView {
 extension String {
     var length: Int {
         return characters.count
-    }
-    
-    func height(withConstrainedWidth width: CGFloat, font: UIFont) -> CGFloat {
-        let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
-        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSFontAttributeName: font], context: nil)
-        
-        return ceil(boundingBox.height)
     }
 }
